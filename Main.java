f0bc75601cc9949faeff6407dc131ccd2a9b061c
BEN P JACOB <benjpns@gmail.com>
2022-07-04 10:17:43 -0500

    Generics

--- Main.java
+++ Main.java
@@ -6,20 +6,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         runTimeParameter(args);
-        getUserInput();
     }
     public static void runTimeParameter(String[] args){
-
        String mainMenu = args[0].toUpperCase(Locale.ROOT);
+        Runtime type = Runtime.DATATYPE;
+        boolean matchEnum = type.getLine().equals(args[1]) ||
+                type.getWord().equals(args[1]) ||
+                type.getLongs().equals(args[1]);
        try {
            switch (mainMenu){
                case "DATATYPE":
-                   Runtime type = Runtime.DATATYPE;
-                   boolean matchEnum = type.getLine().equals(args[1]) ||
-                           type.getWord().equals(args[1]) ||
-                           type.getLongs().equals(args[1]);
                    if (matchEnum){
-                       new UserInput<>(args[1]);
+                       new UserInput(args[1]);
                    } else {
                        System.out.println("Check runtime parameter typo.");
                    }
@@ -37,8 +35,5 @@ public class Main {
        }
     }
 
-    public static void getUserInput(){
-        Scanner input = new Scanner(System.in);
 
-    }
 }
--- UserInput.java
+++ UserInput.java
@@ -1,12 +1,24 @@
 package sorting;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class UserInput<T>{
-    T usersType;
-    public UserInput(T usersType){
-        this.usersType = usersType;
+public class UserInput <T>{
+
+    ArrayList<T> input = new ArrayList<>();
+
+
+    //recursion
+    public UserInput(String sortType){
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            input.add((T) scanner.next());
+        }
+
     }
+
 
 
 }
