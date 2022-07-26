cd045ac61710e62bdd6393c32eb06e656cfe22ee
BEN P JACOB <benjpns@gmail.com>
2022-07-26 09:28:16 -0500

    Generics

---
+++ SortInterface.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface SortInterface {
+    int TotalNumbers();
+}
+
+ interface BigNumber {
+    int Large();
+}
+
+interface BigString {
+    String bString();
+}
+
+interface SortedData {
+    void SortData();
+}
+
+interface Times {
+    int times();
+}
+
+interface Percentage {
+    double percent();
+}
+
+
