d9bddd8bc7e1323c3c88230dfe1e286719eb0f40
BEN P JACOB <benjpns@gmail.com>
2022-07-28 12:37:25 -0500

    Generics

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main {
                     type.getWord().equals(args[1]) ||
                     type.getLongs().equals(args[1]);
         } catch (NullPointerException e) {
+            //UPDARE
             matchSort = type.getSort().equals(args[1]);
         }
 
