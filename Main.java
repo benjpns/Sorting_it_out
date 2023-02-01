5a8caa078b985f9ea63641722280d0560232c9ae
BEN P JACOB <benjpns@gmail.com>
2023-02-01 16:41:58 -0600

    Start over with clean and concise code

--- Main.java
+++ Main.java
@@ -1,67 +1,14 @@
 package sorting;
 
-import java.util.ArrayList;
-import java.util.Locale;
+import java.util.*;
 
 public class Main {
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        try {
-            if (validateUserArgs(args[0], runTimeParameter(args))) {
-                processAndPrint(runTimeParameter(args));
-            }
-        } catch (IndexOutOfBoundsException | IllegalArgumentException e) {
-            System.out.println("Something went wrong.");
+        while (scanner.hasNextLong()) {
+            long number = scanner.nextLong();
+            // write your code here
         }
-
-    }
-
-    public static String runTimeParameter(String[] args) {
-        if (args[0].equals("-sortIntegers")) {
-            return "SORTINTEGERS";
-        }
-        String oneLineMenu;
-        StringBuilder menuMerge = new StringBuilder();
-        for (String s : args) {
-            menuMerge.append(s);
-        }
-        oneLineMenu = String.valueOf(menuMerge).toUpperCase(Locale.ROOT).replaceAll("-", "");
-        return oneLineMenu;
-    }
-
-    public static void processAndPrint(String oneLineMenu) {
-        Type type = new Type(oneLineMenu);
-        System.out.println(type.toString(oneLineMenu));
-    }
-
-    public static boolean validateUserArgs(String args, String oneLineMenu) {
-        String stripArgs = args.toUpperCase(Locale.ROOT).replaceAll("-", "");
-        Runtime enumType = Runtime.valueOf(stripArgs);
-        ArrayList<String> enumArray = new ArrayList<>();
-        enumArray.add(enumType.getLine());
-        enumArray.add(enumType.getLongs());
-        enumArray.add(enumType.getWord());
-        enumArray.add(enumType.getLineSort());
-        enumArray.add(enumType.getWordSort());
-        enumArray.add(enumType.getLongSort());
-        enumArray.add(enumType.getNa());
-        //clear null values in enumArray list;
-        while (enumArray.remove(null)) {
-
-        }
-        boolean pass = false;
-        try {
-            for (String s : enumArray) {
-                pass = s.equals(oneLineMenu);
-                if (pass) {
-                    break;
-                }
-            }
-            return pass;
-        } catch (NullPointerException e) {
-            return false;
-
-        }
-
     }
 }
