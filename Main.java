012a5bd18326955e894333aaa31c942d7697726c
BEN P JACOB <benjpns@gmail.com>
2023-02-02 11:33:29 -0600

    Start over with clean and concise code

--- Main.java
+++ Main.java
@@ -5,10 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Long> numbers = new ArrayList<>();
 
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            numbers.add(number);
         }
     }
 }
