3773c7575f3055fdc56c97ae9fea94c4775ea704
BEN P JACOB <benjpns@gmail.com>
2023-02-02 19:31:51 -0600

    Start over with clean and concise code

--- Main.java
+++ Main.java
@@ -6,10 +6,15 @@ public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Long> numbers = new ArrayList<>();
-
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
             numbers.add(number);
         }
+
+        long max = Collections.max(numbers);
+        int maxFrequency = Collections.frequency(numbers, max);
+
+        System.out.println("Total numbers: " + numbers.size());
+        System.out.printf("The greatest number: %d (%d time(s))", max, maxFrequency);
     }
 }
